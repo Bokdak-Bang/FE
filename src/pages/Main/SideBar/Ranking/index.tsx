f114157e38e373512808a26992f15403d4c8a5dd
JungSoEun <85235356+ssilver01@users.noreply.github.com>
2024-05-05 15:24:47 +0900

    feat: Ranking 컴포넌트 퍼블리싱 구현

--- src/pages/Main/SideBar/Ranking/index.tsx
+++ src/pages/Main/SideBar/Ranking/index.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import styles from './Ranking.module.scss';
+import { LeftArrow } from 'assets';
+import Rank from 'components/Rank';
+import { useNavigate } from 'react-router-dom';
 
 const Ranking = () => {
-  return <div>Ranking</div>;
+  const navigate = useNavigate();
+  const handleBackBtn = () => {
+    console.log('back');
+    window.location.reload();
+  };
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <LeftArrow style={{ cursor: 'pointer' }} onClick={handleBackBtn} />
+        <div className={styles.title}>중요도를 고려한 동네 순위예요!</div>
+      </div>
+      <div className={styles.container}>
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+        <Rank />
+      </div>
+    </>
+  );
 };
 
 export default Ranking;
