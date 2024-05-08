5766b56dd3ddf595f5adb782afb29dc99fa445d9
hyunn522 <tjgus52213@gmail.com>
2024-05-09 03:50:48 +0900

    feat: 로그인 및 세션 구현

---
+++ src/hooks/SignInHooks.ts
@@ -0,0 +1,25 @@
+import { postSignIn } from 'seohyun-apis/SignInApi';
+
+// 로그인 세션 저장
+export const setLoginSession = (email: string, password: string): boolean => {
+  const sessionStorage = window.sessionStorage;
+
+  // email, password로 로그인 요청 -> 토큰 return
+  postSignIn(email, password).then((res) => {
+    if (res.isSuccess) {
+      sessionStorage.setItem('token', res.data.accessToken);
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return false;
+};
+
+// 토큰 가져오기
+export const getLoginToken = () => {
+  const sessionStorage = window.sessionStorage;
+
+  sessionStorage.getItem('token');
+};
