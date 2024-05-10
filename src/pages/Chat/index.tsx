cd0d3b7d9821611ec5a3c5b097e3fa25407088fe
hyunn522 <tjgus52213@gmail.com>
2024-05-10 14:32:27 +0900

    feat: chatting page

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,6 +1,7 @@
 import { getLoginToken } from 'hooks/SignInHooks';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import styles from './Chat.module.scss';
 
 const Chat = () => {
   const navigator = useNavigate();
@@ -11,7 +12,14 @@ const Chat = () => {
       navigator('/signin');
     }
   }, []);
-  return <div>Chat</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>채팅상담</div>
+      <div className={styles.content}>
+        나의 동네를 찾고 공인중개사와 상담을 진행해보세요!
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
