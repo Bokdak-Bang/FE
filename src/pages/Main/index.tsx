1f70defb13a00ce9e0a5c71a5485a3ecf4ab3429
hyunn522 <tjgus52213@gmail.com>
2024-05-11 16:01:34 +0900

    fix: lint error debugging

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Map from 'components/Map';
 import SideBar from './SideBar';
 import styles from './Main.module.scss';
